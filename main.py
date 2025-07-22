425c7812169001c94a13cdb7041ece1f4778503d
bruvanessa <brunavanessa.n@gmail.com>
2025-07-22 11:42:58 -0300

    Primeiro Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+print('Setup Start')
+pygame.init()
+window = pygame.display.set_mode(size=(600,480))
+print('Setup End')
+
+print('Loop Start')
+while True:
+    # Check por todos os eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() # fechar a janela
+            quit() # encerrar o pygame
